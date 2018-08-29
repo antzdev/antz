bc82b2c6035d12104f1895f49bfee66056297fa0
MarchuanVan Der Merwe <marchuanv@gmail.com>
2018-08-29 19:10:57 +0200

    Update index.js - Codepanel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path=require('path');
 const app = express();
 const port= process.env.PORT || 3000;
 
-express.mime.type['bin'] = 'application/octet-stream';
+express.static.mime.define({'application/octet-stream': ['bin']});
 
 app.use('/', express.static(__dirname));
 
